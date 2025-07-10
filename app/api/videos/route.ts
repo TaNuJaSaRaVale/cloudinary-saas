7a0286202d9aec968bdf93a784f862c5db4832be
Tanuja Saravale <saravaletanuja18@gmail.com>
2025-07-10 21:22:33 +0530

    prisma setup

---
+++ app/api/videos/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server"
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET(request: NextRequest) {
+    try{
+        const videos = await prisma.video.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+        return NextResponse.json(videos);
+    } catch (error) {
+        return NextResponse.json({ error: "error fetching videos" }, { status: 500 });
+    }finally{
+        await prisma.$disconnect();
+    }
+}
+
